b95a5a465cd5cf8846087e1e32f74d88ef137e97
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-02-16 02:29:42 +0700

    add: introduce prettier and react-navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import Feed from "./screens/Feed";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+
+type RootStackParamList = {
+  Home: undefined;
+  Profile: { userId: string };
+  Feed: { sort: "latest" | "top" } | undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="Home">
+        <RootStack.Screen name="Home" component={Home} />
+        <RootStack.Screen
+          name="Profile"
+          component={Profile}
+          initialParams={{ userId: "dog" }}
+        />
+        <RootStack.Screen name="Feed" component={Feed} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
