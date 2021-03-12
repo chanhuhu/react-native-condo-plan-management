9daa4aead6460d5eb55646dbb1b1b42ce56c2e76
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-03-13 00:26:32 +0700

    add: component/index

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import CameraPreview from "./CameraPreview";
 import FAB from "./FAB";
+import FloorPlan from "./FloorPlan";
 import ImagesPicker from "./ImagesPicker";
 
-export { CameraPreview, FAB, ImagesPicker };
+export { CameraPreview, FAB, ImagesPicker, FloorPlan };
